26fd5dbb1d46c1e01c6dcfc5bc5ef65d8b72d9df
ottobonilla95 <ottobonilla.dev@gmail.com>
2025-10-28 16:11:58 +0800

    sdfds

--- next.config.js
+++ next.config.js
@@ -1,3 +1,5 @@
+const withNextIntl = require('next-intl/plugin')('./i18n.ts');
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
@@ -42,4 +44,4 @@ const nextConfig = {
   }
 };
 
-module.exports = nextConfig;
+module.exports = withNextIntl(nextConfig);
