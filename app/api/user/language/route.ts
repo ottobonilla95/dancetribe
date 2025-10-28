442b1e7891c00bfa70427059d00eb567958209d5
ottobonilla95 <ottobonilla.dev@gmail.com>
2025-10-28 16:05:29 +0800

    sdfds

--- app/api/user/language/route.ts
+++ app/api/user/language/route.ts
@@ -1,6 +1,6 @@
 import { NextRequest, NextResponse } from 'next/server';
 import { getServerSession } from "next-auth";
-import { authOptions } from "@/app/api/auth/[...nextauth]/route";
+import { authOptions } from "@/libs/next-auth";
 import connectMongo from "@/libs/mongoose";
 import User from "@/models/User";
 
--- i18n.ts
+++ i18n.ts
@@ -1,6 +1,7 @@
 import { getRequestConfig } from 'next-intl/server';
 
 export default getRequestConfig(async ({ locale }) => ({
+  locale,
   messages: (await import(`./messages/${locale}.json`)).default
 }));
 
