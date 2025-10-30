e662a105ef1464c33769d3e9dff903ad3131a767
ottobonilla95 <ottobonilla.dev@gmail.com>
2025-10-30 12:08:56 +0800

    sdf

--- components/PresenceTracker.tsx
+++ components/PresenceTracker.tsx
@@ -1,15 +1,12 @@
 "use client";
 
 import { usePresence } from '@/hooks/usePresence';
-import { useSession } from 'next-auth/react';
 
 /**
  * Silent component that tracks user presence in Firebase
  * Add this to your root layout to track all logged-in users
  */
 export default function PresenceTracker(): null {
-  const { data: session } = useSession();
-  
   // This hook automatically tracks presence
   usePresence();
 
